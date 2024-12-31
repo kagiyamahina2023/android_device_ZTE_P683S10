2d78aac66b4d7102f37db416f22d28574fb8065b
Kagiyamahina <kagiyamahina2023@gmail.com>
2024-12-31 15:22:53 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,6 +7,12 @@
 
 LOCAL_PATH := device/zte/P683S10
 # A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS += \
+    boot \
+    system \
+    vendor \
+    
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
